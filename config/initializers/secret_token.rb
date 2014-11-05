11c8caa6af4ed3ad91d83e47c93a041c067dfff5
aditya_healthgraph <aditya.tagirisa@healthgraph.in>
2014-11-06 01:24:55 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthExp::Application.config.secret_key_base = '83eb30c672d6b1e05daa9f1e83c75f1155af60a7e64340f593a5c55fc995852e3574116fe4229b6277961bc079337aaf26305f7544a75c5653d269664de25506'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuthExp::Application.config.session_store :cookie_store, key: '_auth_exp_session'
